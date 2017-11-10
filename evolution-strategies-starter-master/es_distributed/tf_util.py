ebfddd7c8f606478f8fab388e1d1971dfc446057
Alexey Ignatov <Alexey.Ignatov@kaspersky.com>
2017-11-10 16:16:31 +0300

    FIX: tensorflow 1 adopted

--- evolution-strategies-starter-master/es_distributed/tf_util.py
+++ evolution-strategies-starter-master/es_distributed/tf_util.py
@@ -28,7 +28,7 @@ def max(x, axis=None, keepdims=False):
 def min(x, axis=None, keepdims=False):
     return tf.reduce_min(x, reduction_indices=None if axis is None else [axis], keep_dims = keepdims)
 def concatenate(arrs, axis=0):
-    return tf.concat(axis, arrs)
+    return tf.concat(arrs, axis )
 def argmax(x, axis=None):
     return tf.argmax(x, dimension=axis)
 
@@ -179,7 +179,7 @@ def intprod(x):
 
 def flatgrad(loss, var_list):
     grads = tf.gradients(loss, var_list)
-    return tf.concat(0, [tf.reshape(grad, [numel(v)])
+    return tf.concat( [tf.reshape(grad, [numel(v)], 0)
         for (v, grad) in zip(var_list, grads)])
 
 class SetFromFlat(object):
@@ -202,7 +202,7 @@ class SetFromFlat(object):
 
 class GetFlat(object):
     def __init__(self, var_list):
-        self.op = tf.concat(0, [tf.reshape(v, [numel(v)]) for v in var_list])
+        self.op = tf.concat([tf.reshape(v, [numel(v)]) for v in var_list], 0)
     def __call__(self):
         return get_session().run(self.op)
 
