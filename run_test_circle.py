ff345a0d3099fba67e12824e71d3f717fea744cf
Alexey Ignatov <Alexey.Ignatov@kaspersky.com>
2017-11-15 19:46:30 +0300

    FIX bash wrapper

--- run_test_circle.py
+++ run_test_circle.py
@@ -5,11 +5,33 @@ from pathlib import Path
 import subprocess
 import os
 import errno
+import glob
+import os
 
-bashCommand = "squeue  -p test | grep ignatov"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 
-output, error = process.communicate()
-print(str(output))
+master_ip_path = '/mnt/data/users/dm4/vol12/ignatovalexey_1956/_scratch/master_ip.txt'
+sqCommand = "squeue  -p test | grep ignatov"
+run_on_test = 'sbatch -p test -n128 run python /mnt/data/users/dm4/vol12/ignatovalexey_1956/_scratch/es_rl_experiments/run_es.py'
 
+#sqCommand = "ls | grep e"
+try:
+    procs = len(subprocess.check_output(sqCommand, shell=True   ).splitlines())
+except subprocess.CalledProcessError:
+    procs = 0
+
+print(procs)
+if procs == 0:
+
+    subprocess.check_output('rm /mnt/data/users/dm4/vol12/ignatovalexey_1956/_scratch/tmp/*', shell=True)
+
+    try:
+        os.remove(master_ip_path)
+    except OSError:
+        pass
+
+    subprocess.check_output('run_on_test', shell=True)
+#output, error = process.communicate()
+#print(str(output))
+
+#process = subprocess.Popen(bashCommand.split(), stdout=stdout, shell=True    )
 #process = subprocess.Popen(bashCommand.split(), stdout=stdout, shell=True    )
